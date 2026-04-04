123fe03471be0c70732732712b2068d46f1ddc75
kers0new <kersw@icloud.com>
2026-04-04 13:20:46 -0400

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,1 +1,17 @@
+const { REST, Routes, SlashCommandBuilder } = require('discord.js');
 
+const commands = [
+  new SlashCommandBuilder().setName('ping').setDescription('Test command')
+];
+
+const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+
+(async () => {
+  try {
+    await rest.put(
+      Routes.applicationCommands('1490036330296643584'),
+      { body: commands }
+    );
+    console.log('Commands registered');
+  } catch (err) { console.error(err); }
+})();
