074d8b24e02810933f64350961e703f940d44069
kers0new <kersw@icloud.com>
2026-04-04 13:19:27 -0400

    Set up Discord bot with express server

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+const express = require('express');
 
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+// Keep-alive server
+const app = express();
+app.get('/', (req, res) => res.send('Bot is alive!'));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+// Bot login
+client.once('ready', () => console.log(`Logged in as ${client.user.tag}`));
+client.login(process.env.TOKEN);
